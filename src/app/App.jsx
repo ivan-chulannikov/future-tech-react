7bc500ed5ac41c2813abf955218db06ef508e928
ivan <ivannovi12ko@gmail.com>
2026-06-11 16:21:32 +0300

    migrate project to TypeScript

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,6 @@
 
 import "./styles/styles.scss";
-import { Home } from "@/pages/Home";
+
 import { AppRouter } from "./router/AppRouter";
 
 function App() {
--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -7,10 +7,36 @@ import { ResourcesSection } from "@/widgets/ResourcesSection";
 import { ReviewsSection } from "@/widgets/ReviewsSection";
 import { AboutSection } from "@/widgets/AboutSection";
 import { homePostsSection } from "../model/postSection";
+<<<<<<< Updated upstream:src/pages/Home/ui/Home.jsx
 import { posts } from "@/entities/post";
 
 const Home = () => {
   const homePosts = posts.filter((post) => post.showOnHome);
+=======
+
+import { fetchPosts } from "@/entities/post/api/postApi";
+import { useEffect, useState } from "react";
+import type { Post } from "../../../entities/post/model/types";
+const Home = () => {
+  const [posts,setPosts] = useState<Post[]>([])
+  const [error,setError] = useState(false)
+  
+  const loadPosts = async () => {
+    try {
+      setError(false)
+      const data = await fetchPosts()
+
+      setPosts(data)
+    } catch(error) {
+      console.error(error)
+      setError(true)
+
+    } 
+  }
+  useEffect(() =>{
+    loadPosts()
+  },[])
+>>>>>>> Stashed changes:src/pages/Home/ui/Home.tsx
   return (
     <>
       <Header />
