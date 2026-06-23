f9bedc27d20a7f1200183560eb1b0ae32e43af47
ivan <ivannovi12ko@gmail.com>
2026-06-23 22:28:08 +0300

    feat(auth): add auth selectors

--- src/features/auth/model/selectors.ts
+++ src/features/auth/model/selectors.ts
@@ -1,3 +1,6 @@
 import type { RootState } from '@/app/store/store';
 
 export const selectToken = (state: RootState) => state.auth.accessToken;
+export const selectUser = (state: RootState) => state.auth.user;
+export const selectIsAuthenticated = (state: RootState) =>
+  state.auth.user !== null && state.auth.accessToken !== null;
